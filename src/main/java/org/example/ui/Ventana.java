19366084f1ebe532bfc4774935d4a668ffc5bd5e
alvaro <alvaro.bonilla.lledo@gmail.com>
2023-10-06 14:03:15 +0200

    ventana hecha pero no ajustada

---
+++ src/main/java/org/example/ui/Ventana.java
@@ -0,0 +1,31 @@
+package org.example.ui;
+
+import javax.swing.*;
+
+public class Ventana {
+    private JPanel panel;
+    private JTextField texto;
+    private JButton a1Button;
+    private JButton a4Button;
+    private JButton a7Button;
+    private JButton a2Button;
+    private JButton a5Button;
+    private JButton a8Button;
+    private JButton a3Button;
+    private JButton a6Button;
+    private JButton a9Button;
+    private JPanel columna1;
+    private JPanel columna2;
+    private JPanel columna3;
+    private JButton sumar;
+    private JButton restar;
+    private JButton multiplicar;
+    private JButton dividir;
+    private JPanel operaciones;
+    private JButton a0Button;
+    private JButton igual;
+    private JButton punto;
+    private JPanel numeros;
+
+
+}
