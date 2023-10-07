1fb22ff560df854a4fc99c3798547a04c303ae2f
alvaro <alvaro.bonilla.lledo@gmail.com>
2023-10-07 14:20:23 +0200

    4*4 en el centro

--- src/main/java/org/example/ui/Ventana.java
+++ src/main/java/org/example/ui/Ventana.java
@@ -1,8 +1,11 @@
 package org.example.ui;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.OptionalDataException;
 
-public class Ventana {
+public class Ventana extends JFrame{
     private JPanel panel;
     private JTextField texto;
     private JButton a1Button;
@@ -26,6 +29,29 @@ public class Ventana {
     private JButton igual;
     private JButton punto;
     private JPanel numeros;
+    private JPanel arriba;
+    private JPanel abajo;
+
+
+    public Ventana(){
+        this.setContentPane(panel);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(300,300);
+        setTitle("Calculadora");
+        setLocationRelativeTo(null);
+        setResizable(false);
+
+    }
+
+    private Integer pulsarNumero(Integer n) {
+        System.out.println("Boton "+n+" pulsado");
+        return n;
+    }
+
+
+    public void load(){
+        this.setVisible(true);
+    }
 
 
 }
